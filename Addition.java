24b73dcb1bc148c2420aa061fba985cb4e275d43
murugadass-b <murugadassbala@gmail.com>
2024-10-05 20:47:34 +0530

    updated addition.java

--- Addition.java
+++ Addition.java
@@ -4,6 +4,7 @@ class Addition {
     public static void main(String args[]){
         int a =19;
         int b =10;
-        System.out.println(a+b);
+        int ans = a+b;
+        System.out.println(ans);
     }
 }
